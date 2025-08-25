dde36ed88773e823af2026d02473e35cb2b25065
Kunwar-awadhiya <kunwarawadhiya@gmail.com>
2025-08-25 12:52:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navbar from './components/navbar/Navbar'
+import Hero from './components/hero-Section/hero'
+import Footer from './components/footer/Footer'
+
+function App() {
+
+  return (
+    <>
+     <Navbar/>
+     <Hero/>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/hero-Section/Hero.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section
+      className="mx-auto flex items-center justify-between px-16"
+      style={{
+        background:
+          "linear-gradient(149.47deg, #0059FF -21.36%, #000000 68.05%)",
+      }}
+    >
+      {/* Left */}
+      <div className="flex flex-col justify-center text-white max-w-2xl pl-55">
+        <h1 className="text-5xl font-bold leading-tight">
+          Reliable, Secure & Scalable Hosting Solutions for your Businesses
+        </h1>
+        <p className="mt-6 text-gray-300 leading-relaxed">
+          From datacentre co-location to fully managed VPS and web hosting, 9
+          Hosting delivers future-ready infrastructure with great support you
+          can count on.
+        </p>
+        <button className="mt-8 bg-blue-600 hover:bg-blue-700 text-white font-semibold px-8 py-3 rounded-lg w-fit">
+          Get Started
+        </button>
+      </div>
+
+      {/* Right */}
+      <div className="flex justify-center items-center">
+        <img
+          src="/public/images/Group 208.png"
+          alt="Servers"
+          className="w-[1000px] h-auto"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
